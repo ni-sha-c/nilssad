da4363651ccc6d3654857a828607cbff599a14e3
Nisha Chandramoorthy <nishac92@gmail.com>
2017-06-16 11:36:42 -0500

    Compute inhomogeneous tangent equation with different initial conditions both by directly solving the inhomogeneous tangent equation and by applying OpenAD on the primal, with perturbed initial conditions.

--- driver.f90
+++ driver.f90
@@ -7,42 +7,39 @@ program driver
 	type(active) :: x, y
 	double precision, dimension(3) :: X0
 	double precision, dimension(3,1) :: v0,v
-	double precision :: dzds_fd
+	double precision :: dzds_t
 	integer :: t
-	double precision :: zs, z0
+	double precision :: zs, z0, ds
+	ds = 0.005d0
 	x%v=2.8D1
 	y%d=1.0D0
 	our_rev_mode%tape=.TRUE.
-	call head(x,y)
-	print *, 'driver running for x =',x%v
-	print *, '            yields y =',y%v,' dy/dx =',x%d
-
-	!check using tangent equation
 	X0(1) = x%v
 	X0(2) = x%v
 	X0(3) = x%v
-	v0(1,1) = 1.d0
-	v0(2,1) = 1.d0
+	!Get on the attractor.
+	do t = 1, 10000, 1
+		call Xnp1(X0,X0,x%v)
+	end do	
+
+	v0(1,1) = 2.d0
+	v0(2,1) = 0.d0
 	v0(3,1) = 1.d0
-	do t = 1, 1000, 1
+
+
+	call head(x,y,X0,v0)
+	print *, 's =',x%v
+	print *, 'From AD, dzbar/ds = ',x%d
+
+	!check using tangent equation
+	do t = 1, 100, 1
 		call rk45_full(X0,v0,v)
 		call Xnp1(X0,X0,x%v)
 		v0 = v
 	end do	
 
 	z0 = X0(3)
-	dzds_fd = v(3,1)
-	print *,'dy/dx from the tangent equation is: ', dzds_fd
+	dzds_t = v(3,1)
+	print *,'From the tangent equation, dzbar/dx = ', dzds_t
 
-	!Check using finite difference	
-	X0(1) = x%v
-	X0(2) = x%v
-	X0(3) = x%v
-
-	do t = 1, 1000, 1
-		call Xnp1(X0,X0,x%v+0.0005)
-	end do	
-	zs = X0(3)
-	print *,'dy/dx from finite difference is: ', (zs-z0)/0.0005
-	print *, '    dz^1000/dz = ',1.D0*dzds_fd-x%d
 end program driver
