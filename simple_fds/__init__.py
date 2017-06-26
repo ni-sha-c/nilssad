6deb2f9fbce59ab9570b7bde3f5c32b6b0d87de6
Nisha Chandramoorthy <nishac92@gmail.com>
2017-06-26 09:18:58 -0500

    simple_fds module getting ready!

---
+++ simple_fds/__init__.py
@@ -0,0 +1,6 @@
+from . import fds as core
+from . import lsstan
+from . import timedilation
+from . import segment
+from . import timeseries
+from .fds import shadowing, continue_shadowing, lss_gradient
