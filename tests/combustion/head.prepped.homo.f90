985552dc7469388e0c86d7a3287b9111a1965925
Nisha Chandramoorthy <nishac92@gmail.com>
2017-07-10 16:35:15 -0500

    wrote head for homogenous tangents

--- tests/combustion/head.prepped.homo.f90
+++ tests/combustion/head.prepped.homo.f90
@@ -1,26 +1,40 @@
-subroutine head_homogeneous(dtau,tau,y,X0,v0,nSteps)
+subroutine head_homogeneous(dtau,inttau,y,X0,v0,nSteps)
 	use combustion_juniper
 	implicit none 
 	double precision, dimension(d) :: X
 	double precision, dimension(d) :: Xnp1_res
-	double precision, intent(in) :: tau
+	integer, intent(in) :: inttau
 	double precision, dimension(d), intent(in) :: X0, v0
 	double precision, dimension(d), intent(out) :: y
-	integer :: t,t1
+	integer :: t, t1, t2
 	integer, intent(in) :: nSteps
 	double precision, intent(in) :: ds
-
+	double precision, dimension(d,inttau) :: Xtmtau = 0.d0
 
 
 !$openad INDEPENDENT(ds)
+
+	
 	do t = 1, d, 1 
 		X(t) = X0(t) + v0(t)*dtau
 	end do
-	
-	do t = 1, nSteps, 1
-		call Xnp1(X,Xnp1_res,Xtmtau)
+
+	do t = 1, inttau, 1
+		call Xnp1(X,Xnp1_res,0.d0)
 		do t1 = 1, d, 1
-			X(t1) = Xnp1_res(t1)		
+			X(t1) = Xnp1_res(t1)
+			Xtmtau(t1,t) = Xnp1_res(t1)		
+		end do
+	end do
+	
+	do t = 1, nSteps - inttau, 1
+		call Xnp1(X,Xnp1_res,Xtmtau(:,mod(t,inttau)))
+		do t1 = 1, d, 1
+			X(t1) = Xnp1_res(t1)
+			do t2 = 1, inttau-1, 1
+				Xtmtau(t1,t2) = Xtmtau(t1,t2+1)		
+			end do
+			Xtmtau(t1,inttau) = Xnp1_res(t1)
 		end do
 	end do
 	do t = 1, d, 1
