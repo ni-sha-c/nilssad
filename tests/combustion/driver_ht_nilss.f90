a83c7f517862323d279403b60b022a91206aaf33
Nisha Chandramoorthy <nishac92@gmail.com>
2017-07-10 15:54:03 -0500

    what should the nonlinear initial conditions be

--- tests/combustion/driver_ht_nilss.f90
+++ tests/combustion/driver_ht_nilss.f90
@@ -1,7 +1,7 @@
 program driver
 	use OAD_active
 	use OAD_rev
-	use lorenz63_passive
+	use combustion_juniper_passive
 	implicit none 
 	external head_homogeneous
 	type(active), dimension(:), allocatable :: x
--- tests/combustion/head.prepped.homo.f90
+++ tests/combustion/head.prepped.homo.f90
@@ -1,28 +1,29 @@
-subroutine head_homogeneous(ds,s,y,X0,v0,nSteps)
-	use Lorenz63
+subroutine head_homogeneous(dtau,tau,y,X0,v0,nSteps)
+	use combustion_juniper
 	implicit none 
-	double precision, dimension(3) :: X
-	double precision, dimension(3) :: Xnp1_res
-	double precision, intent(in) :: s
-	double precision, dimension(3), intent(in) :: X0, v0
-	double precision, dimension(3), intent(out) :: y
-	integer :: t
+	double precision, dimension(d) :: X
+	double precision, dimension(d) :: Xnp1_res
+	double precision, intent(in) :: tau
+	double precision, dimension(d), intent(in) :: X0, v0
+	double precision, dimension(d), intent(out) :: y
+	integer :: t,t1
 	integer, intent(in) :: nSteps
 	double precision, intent(in) :: ds
 
 
 
-!$openad INDEPENDENT(ds) 
-	X(1) = X0(1) + v0(1)*ds
-	X(2) = X0(2) + v0(2)*ds
-	X(3) = X0(3) + v0(3)*ds
-	do t = 1, nSteps, 1
-		call Xnp1(X,Xnp1_res,s)
-		X(1) = Xnp1_res(1)
-		X(2) = Xnp1_res(2)
-		X(3) = Xnp1_res(3)
+!$openad INDEPENDENT(ds)
+	do t = 1, d, 1 
+		X(t) = X0(t) + v0(t)*dtau
 	end do
-	do t = 1, 3, 1
+	
+	do t = 1, nSteps, 1
+		call Xnp1(X,Xnp1_res,Xtmtau)
+		do t1 = 1, d, 1
+			X(t1) = Xnp1_res(t1)		
+		end do
+	end do
+	do t = 1, d, 1
 		y(t) = X(t)
 	end do
 	!y(1) = 1.d0
