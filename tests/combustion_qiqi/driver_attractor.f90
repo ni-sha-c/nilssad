1d18283657da66bcd49c48fa88c262231adcc870
Nisha Chandramoorthy <nishac92@gmail.com>
2017-07-29 18:53:46 -0500

    get long-time averaged objective of King's model implemented through the advection equation.

---
+++ tests/combustion_qiqi/driver_attractor.f90
@@ -0,0 +1,73 @@
+program flow
+	use combustion_qiqi_passive
+	implicit none 
+	double precision, dimension(d) :: X0, X1, zeroarray, Xnp1_res
+	double precision, dimension(:), allocatable :: J
+	integer :: t, nSteps, t1, t2
+	double precision:: param_active, tau
+	double precision, dimension(N_p) :: params
+	double precision, dimension(N_p-1) :: params_passive
+	character(len=128) :: arg
+
+
+
+
+	
+	if (command_argument_count() .ne. 1) then
+        print *, "Need number of time steps"
+        call exit(-1)
+    end if
+
+	call get_command_argument(1, arg)
+    Read(arg, '(i10)') nSteps
+
+	allocate(J(nSteps))
+
+
+	Open(1, file="input_attractor.bin", form="unformatted", access="stream", &
+            status="old", convert='big_endian')
+    Read(1) X0
+    Close(1)
+
+	Open(1, file="param.bin", form="unformatted", access="stream", &
+            status="old", convert='big_endian')
+	do t = 1, N_p, 1
+    	Read(1) params(t)
+	end do
+    Close(1)
+	param_active = params(1)
+	params_passive = params(2:N_p)
+	tau = params_passive(N_p-1)
+	
+	do t = 1, d, 1
+		X1(t) = X0(t) 
+	end do
+
+	
+	Open(1, file="pf_history.bin", form="unformatted", access="stream", &
+         status='replace', convert='big_endian')
+
+    do t = 1, nSteps, 1
+		call Xnp1(X1,Xnp1_res,Xtmtau(:,counter),param_active,params_passive)
+		do t1 = 1, d, 1
+			X1(t1) = Xnp1_res(t1)
+		end do
+		call Objective(Xnp1_res,J(t),param_active,params_passive)
+		if(counter .eq. inttau) then
+			counter = 0
+		end if
+        Write(1) J(t)
+	end do
+
+	
+	!print *, X1
+	
+    Close(1)
+
+    !Open(1, file="objective.bin", form="unformatted", access="stream", &
+    !     status='replace', convert='big_endian')
+    !Write(1) J
+    !Close(1)
+
+
+end program flow
---
+++ tests/combustion_qiqi/plot_attractor.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import shutil
+import tempfile
+import numpy as np
+from scipy.stats import linregress
+from subprocess import *
+import matplotlib.pyplot as plt
+
+my_path = os.path.dirname(os.path.abspath(__file__))
+solver = os.path.join(my_path, 'solver_attractor')
+u0 = np.loadtxt(os.path.join(my_path, 'u0'))
+
+def solve(u, s, nsteps):
+    tmp_path = tempfile.mkdtemp()
+    with open(os.path.join(tmp_path, 'input_attractor.bin'), 'wb') as f:
+        f.write(np.asarray(u, dtype='>d').tobytes())
+    with open(os.path.join(tmp_path, 'param.bin'), 'wb') as f:
+        f.write(np.asarray(s, dtype='>d').tobytes())
+        f.write(np.asarray([0.05,0.865,0.3,0.04], dtype='>d').tobytes())
+    call([solver, str(int(nsteps))], cwd=tmp_path)
+    with open(os.path.join(tmp_path, 'pf_history.bin'), 'rb') as f:
+        out = np.frombuffer(f.read(), dtype='>d')
+    shutil.rmtree(tmp_path)
+    return out
+
+fig, ax = plt.subplots()
+Nsteps = 50000
+Ns = 150
+k = 0
+p = np.zeros(Ns)
+ss = np.linspace(0.005,0.05,Ns)
+for s in ss:
+    eta = np.zeros(20)
+    eta[0] = 2.0
+    eta[10] = 2.0
+    u = np.zeros(Ns)
+    pf_hist = solve(eta,s,Nsteps)
+    print(max(pf_hist),min(pf_hist),np.mean(pf_hist))
+    p[k] = np.mean(pf_hist[5000:])
+    k = k + 1
+
+slope, intercept, r_value, p_value, std_err = linregress(ss, p)
+print("slope is: ",slope) 
+ax.plot(ss,p,"o",linewidth=1.0)
+plt.legend(loc='upper left')
+fig.savefig('../../demo/plots/pf_vs_c1.eps', facecolor=fig.get_facecolor(), edgecolor='none', format='eps', dpi=2000)
